0960e000ec2f6eecaeb02f6e3b01cd1f6ec6c5a7
Yan Ren <yannyren@gmail.com>
2017-12-11 15:14:04 +0000

    initial

---
+++ public/app.js
@@ -0,0 +1,16 @@
+const app = function() {
+  const li1 = document.createElement('li');
+  li1.innerText = "Name: Jack";
+  const li2 = document.createElement('li');
+  li2.innerText = "Favourite food: catfish";
+  const li3 = document.createElement('li');
+  const img = document.createElement('img');
+  img.width = 500;
+  img.src = "";
+  li3.appendChild(img);
+
+  const
+}
+
+
+document.addEventListener("DOMContentLoaded", app);
